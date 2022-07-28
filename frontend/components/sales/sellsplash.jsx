add79e23d6467ad3ec8decbe55810bd79561105c
David-Allen <davida.asencio@gmail.com>
2022-07-28 16:40:16 -0400

    working on sale splash

--- frontend/components/sales/sellsplash.jsx
+++ frontend/components/sales/sellsplash.jsx
@@ -6,9 +6,30 @@ function SellSplash() {
             <div className="sellerlanding">
                 <div className='sellerwrapper'>
                     <h1 className="sellertitle">Explore Your Selling Options</h1>
-                    <p>Wherever you are in the selling journey, new opportunities await.</p>
+                    <br></br>
+                    <p>Wherever you are in the selling journey, new opportunities await 💸</p>
                 </div>
             </div>
+            <div className='sellvaluationcontainer'>
+                <h2 className='sellvaltitle'>Enter Your Address to Unlock Your Owner Dashboard</h2>
+                    <div className='leftblurb'> 
+                        <h3 className='blurbtitle'>
+                            See Your StreetEasy Valuation
+                        </h3>
+                        <p>
+                            Get an instant, hassle-free estimate of your home’s value, powered by the most expansive real estate database in NYC.
+                        </p>
+                    </div>
+                    <div className='rightblurb'> 
+                        <h3 className='blurbtitle'>
+                            
+                        </h3>
+                        <p>
+                          
+                        </p>
+                    </div>
+            </div>
+
         </main>
     )
 }
