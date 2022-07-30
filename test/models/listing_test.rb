4410485ae144c52970500f20046bee556525f539
David-Allen <davida.asencio@gmail.com>
2022-07-30 12:42:25 -0400

    annotated models

--- test/models/listing_test.rb
+++ test/models/listing_test.rb
@@ -1,3 +1,26 @@
+# == Schema Information
+#
+# Table name: listings
+#
+#  id             :bigint           not null, primary key
+#  name           :string           not null
+#  address        :string           not null
+#  neighborhood   :string           not null
+#  zip            :integer          not null
+#  price          :integer          not null
+#  property_type  :string
+#  owner_id       :integer          not null
+#  bedrooms       :integer          not null
+#  bathrooms      :integer          not null
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
+#  lat            :float
+#  lng            :float
+#  description    :text             default(""), not null
+#  rent_bool      :boolean          default(FALSE)
+#  borough        :string           default("Brooklyn")
+#  favorite_count :integer          default(0)
+#
 require 'test_helper'
 
 class ListingTest < ActiveSupport::TestCase
